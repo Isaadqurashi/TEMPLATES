26eee75da327c99a65434f65a8c7b4a476d55734
Saad Khurshid Qurashi <147279806+Isaadqurashi@users.noreply.github.com>
2024-07-22 23:31:22 +0500

    Create start.cpp

---
+++ start.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+#define flo(fc,n) for(int i=c;i<n;i++)
+#define rlo(n,rc) for(int i=n;i>rc;i--)
+#define ll long long
+using namespace std;
+
+void saad(){
+
+}
+
+int main() {
+    ios::sync_with_stdio(false),cin.tie(nullptr);
+    int t;cin>>t;
+    while(t--){
+        saad();
+    }
+}
