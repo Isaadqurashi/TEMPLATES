3cf09e16c33f6dbf62071f46f740772177f8b34d
Saad Khurshid Qurashi <147279806+Isaadqurashi@users.noreply.github.com>
2024-07-30 04:04:12 +0500

    Create basic.cpp

---
+++ basic.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+#define fli(fc,n) for(int i=fc;i<n;i++)
+#define flj(fc,n) for(int j=fc;j<n;j++)
+#define flk(fc,n) for(int k=fc;k<n;k++)
+#define rli(n,rc) for(int i=n;i>rc;i--)
+#define rlj(n,rc) for(int j=n;j>rc;j--)
+#define rlk(n,rc) for(int k=n;k>rc;k--)
+#define ll long long
+#define nl "\n"
+using namespace std;
+
+void saad(){}
+
+int main() {
+    ios::sync_with_stdio(0),cin.tie(0);
+    int t;
+    cin>>t;
+    while(t--){
+        saad();
+    }
+}   
