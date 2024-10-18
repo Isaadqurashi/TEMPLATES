fa50fe86779639cbc4aeeba5df4d92dc4f8f6cdd
Saad Khurshid Qurashi <147279806+Isaadqurashi@users.noreply.github.com>
2024-10-18 14:13:56 +0500

    Update basic.cpp

--- basic.cpp
+++ basic.cpp
@@ -1,7 +1,6 @@
 #include <bits/stdc++.h>
-#define fli(fc,n) for(int i=fc;i<n;i++)
-#define flj(fc,n) for(int j=fc;j<n;j++)
-#define rli(n,rc) for(int i=n;i>rc;i--)
+#define fli(i,fc,n) for(int i=fc;i<n;i++)
+#define rli(i,n,rc) for(int i=n;i>rc;i--)
 #define sz(a) a.size()
 #define ll long long
 #define pb push_back
@@ -15,7 +14,7 @@ void saad(){}
 int main() {
     ios::sync_with_stdio(false),cin.tie(nullptr);
     int t;
-    cin>>t;
+    cin >> t;
     while(t--){
         saad();
     }
