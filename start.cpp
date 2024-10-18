eb1e06234abf8d001972cefa4d947cba1a284bbe
Saad Khurshid Qurashi <147279806+Isaadqurashi@users.noreply.github.com>
2024-10-18 14:13:20 +0500

    Update start.cpp

--- start.cpp
+++ start.cpp
@@ -1,10 +1,6 @@
 #include <bits/stdc++.h>
-#define fli(fc,n) for(int i=fc;i<n;i++)
-#define flj(fc,n) for(int j=fc;j<n;j++)
-#define flk(fc,n) for(int k=fc;k<n;k++)
-#define rli(n,rc) for(int i=n;i>rc;i--)
-#define rlj(n,rc) for(int j=n;j>rc;j--)
-#define rlk(n,rc) for(int k=n;k>rc;k--)
+#define fli(i,fc,n) for(int i=fc;i<n;i++)
+#define rli(i,n,rc) for(int i=n;i>rc;i--)
 #define sz(a) a.size()
 #define ll long long
 #define pb push_back
@@ -13,13 +9,12 @@
 #define nl "\n"
 const int MOD = 1e9+7;
 using namespace std;
-
 void saad(){}
 
 int main() {
     ios::sync_with_stdio(false),cin.tie(nullptr);
     int t;
-    cin>>t;
+    cin >> t;
     while(t--){
         saad();
     }
